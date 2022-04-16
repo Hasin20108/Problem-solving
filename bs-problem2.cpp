6868ba659980ab922f3188ec8ea89d60e1d937c8
Bakhtiar Hasin <s2010876101@ru.ac.bd>
2022-04-16 12:39:13 +0600

    seachCircularArray standard solution

--- bs-problem2.cpp
+++ bs-problem2.cpp
@@ -10,29 +10,27 @@ using namespace std;
 int searchCircularArray(int ar[], int n, int x){
 	int high = n - 1;
 	int low = 0;
-	while(low < high){
-		cout << "running\n";
-		if(ar[high] == x){
-			return high;
-		}
-		if(ar[low] == x){
-			return low;
-		}
+	while(low <= high){
 		int mid = (high + low) / 2;
 		if(ar[mid] == x){
 			return mid;
 		}
-		else if(x > ar[mid] and x <= ar[high]){
-			low = mid + 1;
+		if(ar[mid] <= ar[high]){
+			if(x > ar[mid] and x <= ar[high]){
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
 		}
-		else if(x > ar[mid] and x > ar[high]){
-			high = mid - 1;
-		}
-		else if(x < ar[mid] and x >= ar[low]){
-			high = mid - 1;
-		}
-		else if(x < ar[mid] and x < ar[low]){
-			low = mid + 1;
+		else{
+			if(x >= ar[low] and x < ar[mid]){
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+
 		}
 	}
 	return -1;
